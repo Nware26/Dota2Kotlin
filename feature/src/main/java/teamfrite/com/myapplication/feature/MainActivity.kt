8671eb93b47849447cb5bec768bf7598c8da171a
NicoG26 <43745235+NicoG26@users.noreply.github.com>
2018-11-13 14:29:40 +0100

    First

---
+++ feature/src/main/java/teamfrite/com/myapplication/feature/MainActivity.kt
@@ -0,0 +1,12 @@
+package teamfrite.com.myapplication.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
